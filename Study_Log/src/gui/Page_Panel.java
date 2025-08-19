030570cd063d9875546fd58097bf4bb8c32e825a
Cheyenne Choong <cheyennechoong@gmail.com>
2025-08-19 17:55:24 +0800

    GUI Frames

---
+++ Study_Log/src/gui/Page_Panel.java
@@ -0,0 +1,13 @@
+package gui; /*Package containing the code.*/
+
+/*Import Java API.*/
+import javax.swing.*;
+import java.awt.Color;
+
+public class Page_Panel{ /*Class for creating the base frame.*/
+    JPanel panel = new JPanel();
+    
+    /*Constructor method.*/
+    public Page_Panel() {
+    }
+}
---
+++ Study_Log/src/main/Main.java
@@ -0,0 +1,10 @@
+package main; /*Package containing the code.*/
+
+/*Import API needed for creating the interface.*/
+
+public class Main { /*Main class run.*/
+
+    /*Main method.*/
+    public static void main(String[] args){ 
+    }
+}
